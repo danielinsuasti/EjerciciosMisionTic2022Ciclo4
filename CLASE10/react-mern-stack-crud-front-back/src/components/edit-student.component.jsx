20aafbe8e257eb9ed1264eac543d121940281277
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-11-17 18:38:46 -0500

    Inicio Clase 10, adicion del back al Crud Students

---
+++ CLASE10/react-mern-stack-crud-front-back/src/components/edit-student.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditStudent extends Component{
+    render(){
+        return(
+            <div>
+                <p>Componente editar estudiante en react</p>
+            </div>
+        )
+    }
+}
